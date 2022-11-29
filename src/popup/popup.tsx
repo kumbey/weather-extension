3de24087982637cdf28cb9194c80ee34762fd747
kumurbek <74360101+kumbey@users.noreply.github.com>
2022-11-29 16:40:42 +0800

    added fetch api

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { createRoot } from "react-dom/client";
+import { fetchOpenWeatherData } from "../utils/api";
 import "./popup.css";
 
-const test = (
-  <div>
-    <img src="icon.png" alt="" />
-  </div>
-);
+const App: React.FC = () => {
+  useEffect(() => {
+    fetchOpenWeatherData("Toronto")
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <div>
+      <img src="icon.png" alt="" />
+    </div>
+  );
+};
 
 const container = document.createElement("div");
 document.body.appendChild(container);
 const root = createRoot(container);
 
-root.render(test);
+root.render(<App />);
